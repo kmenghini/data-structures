5bf7207c66ac19598218ff5976648283e2e49beb
kmenghini <kaitlyn.menghini@gmail.com>
2017-11-29 15:24:38 -0800

    Complete stack for functional class

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var key = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+  //save value in storage
+    storage[key] = value;
+  //increment key
+    key++;
   };
 
   someInstance.pop = function() {
+  //decrement key
+    key--;
+  //save value as variable
+    var storageValue = storage[key];
+  //delete value
+    delete storage[key];
+  //return variable
+    return storageValue;
   };
 
   someInstance.size = function() {
+  //return key size
+    return Math.max(key, 0);
   };
 
   return someInstance;
