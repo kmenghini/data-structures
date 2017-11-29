866c084f8f7faca30e1f89c4827bb7d9a66efe02
kmenghini <kaitlyn.menghini@gmail.com>
2017-11-29 15:18:55 -0800

    Add queue for functional class

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,31 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var key = 0;
+  var start = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+  //save value in storage with key
+    storage[key] = value;
+  //increment key
+    key++;
   };
 
   someInstance.dequeue = function() {
+  //save stored value to variable
+
+    var returnValue = storage[start];
+  //delete key value pair
+    delete storage[start];
+  //decrement key
+    start++;
+    return returnValue;
   };
 
   someInstance.size = function() {
+  //return key
+    return Math.max(key - start, 0); 
   };
 
   return someInstance;
